7b9f96d8d44bd98c5b2de744026b86a4c174f6ec
temidayo03 <adesanmitemidayo09@gmail.com>
2020-06-24 22:07:52 +0100

    deploy only build folder

---
+++ build/precache-manifest.9f6b99399414393e012675a44098da46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c546750971b1bb7ad7744fbd04e596",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "418aac7015397b31d619",
+    "url": "/monsters-rolodex/static/css/main.23d20aed.chunk.css"
+  },
+  {
+    "revision": "660d2cd3575831f386b7",
+    "url": "/monsters-rolodex/static/js/2.41654469.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.41654469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418aac7015397b31d619",
+    "url": "/monsters-rolodex/static/js/main.207dfe29.chunk.js"
+  },
+  {
+    "revision": "f4f8959d4dfa7a7b1415",
+    "url": "/monsters-rolodex/static/js/runtime-main.4cc1b440.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-Rolodex/precache-manifest.366d0a823e77829e03d7d74792d58a43.js"
+  "/monsters-rolodex/precache-manifest.9f6b99399414393e012675a44098da46.js"
 );
 
 self.addEventListener('message', (event) => {
